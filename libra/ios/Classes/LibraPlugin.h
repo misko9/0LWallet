1762b19bfc73aa0f9fdb2218ffc9f5c83086180b
Steve Misko <steve@misko.io>
2022-08-02 21:24:49 -0600

    Release candidate 1.3.18

--- libra/ios/Classes/LibraPlugin.h
+++ libra/ios/Classes/LibraPlugin.h
@@ -17,4 +17,15 @@ char *rust_balance_transfer(uint64_t coins,
                             uint64_t sequence_num,
                             const char *dest_addr,
                             const char *mnem);
+char *rust_community_balance_transfer(uint64_t coins, uint64_t sequence_num, const char *dest_addr, const char *mnem);
+char *rust_claim_make_whole(uint64_t sequence_num, const char *mnem);
+uint64_t rust_get_make_whole_credits_from_state(const char *blob);
+bool rust_is_make_whole_claimed_from_state(const char *blob);
+char *rust_get_ancestry_from_state(const char *blob);
+char *rust_get_vouchers_from_state(const char *blob);
+char *rust_get_wallet_type_from_state(const char *blob);
+bool rust_is_validator_from_state(const char *blob);
+bool rust_is_operator_from_state(const char *blob);
+uint64_t rust_get_unlocked_from_state(const char *blob);
+uint64_t rust_get_transferred_from_state(const char *blob);
 void rust_cstr_free(char *s);
--- libra/ios/Classes/SwiftLibraPlugin.swift
+++ libra/ios/Classes/SwiftLibraPlugin.swift
@@ -18,6 +18,17 @@ public class SwiftLibraPlugin: NSObject, FlutterPlugin {
       //rust_add(1,2);
       is_mnem_valid("...");
       rust_balance_transfer(1, 2, "1", "2");
+      rust_community_balance_transfer(1, 2, "1", "2");
+      rust_claim_make_whole(1, "1");
+      rust_get_make_whole_credits_from_state("1");
+      rust_is_make_whole_claimed_from_state("1");
+      rust_get_ancestry_from_state("1");
+      rust_get_vouchers_from_state("1");
+      rust_get_wallet_type_from_state("1");
+      rust_is_validator_from_state("1");
+      rust_is_operator_from_state("1");
+      rust_get_unlocked_from_state("1");
+      rust_get_transferred_from_state("1");
       //rust_cstr_free("1");
       // ...
       // This code will force the bundler to use these functions, but will never be called
